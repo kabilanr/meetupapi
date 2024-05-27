67938b492b8f4914b159e049f79e2e3b377c6912
manju2089 <kabilanr13@gmail.com>
2024-05-27 11:33:25 +0530

    'final_check'

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://meetupapi.onrender.com/','127.0.0.1:8000']
+ALLOWED_HOSTS = ['meetupapi.onrender.com','127.0.0.1:8000']
 
 
 # Application definition
